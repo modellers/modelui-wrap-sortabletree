6d220030f81b4d3113c4ee658f0c385133899830
modellers <115144400+modellers@users.noreply.github.com>
2022-12-29 13:44:29 +0000

    Building lib

--- src/library.js
+++ src/library.js
@@ -3,8 +3,4 @@
  * Notes: https://hinammehra.medium.com/build-a-private-react-component-library-cra-rollup-material-ui-github-package-registry-1e14da93e790
  * */
 
-// export { default as registerComponents } from './components/Components';
 export { default as registerComponents } from './components/Components';
-export { Layout } from './components/Grid/Grid';
-// export { Dialog as Dialog } from './components/Modal/Modal';
-// export { World as World } from './components/Hello';
